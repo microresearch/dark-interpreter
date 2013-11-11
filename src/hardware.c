fd6799698b4a7283fb962fde4191e88e0bc8daf0
Martin Howse <m@1010.co.uk>
2013-11-11 15:59:45 +0000

    removed re-init of ports so now all hardware switch and un/hang works!

--- src/hardware.c
+++ src/hardware.c
@@ -160,13 +160,13 @@ feedback on/off - jackin-> - lm358in->
 
   res= (modder>>5)&3; // 12 bits now lose 5 = 7 bits = 0->25
   res2=(modder>>8); // so now we have 4 bits left = 0->4 options
-  res2=14;
+  //  res2=14;
   res=2;
 
   //unhang
   if (res2!=1 && res2!=2 && hangflag==1){
     hangflag=0;
-    setup_switches();
+    reset_switches();
   }
 
   switch(res){
@@ -204,25 +204,25 @@ feedback on/off - jackin-> - lm358in->
     //question is if really makes sense to unhang _all_
     hangflag=1;
     //  GPIO_Init(GPIOB, &GPIO_InitStructure);
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9 ;
-  GPIO_InitStructure.GPIO_Mode = 0x04; // defined as IN_FLOATING?
-  GPIO_Init(GPIOB, &GPIO_InitStructure);
+      GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9 ;
+      GPIO_InitStructure.GPIO_Mode = 0x04; // defined as IN_FLOATING?
+      GPIO_Init(GPIOB, &GPIO_InitStructure);
 
   // and what to hang on c=8,10,11
-  GPIO_Init(GPIOC, &GPIO_InitStructure);
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_10 | GPIO_Pin_11;
-  GPIO_InitStructure.GPIO_Mode = 0x04; // defined as IN_FLOATING?
-  GPIO_Init(GPIOC, &GPIO_InitStructure);
+    //  GPIO_Init(GPIOC, &GPIO_InitStructure);
+      GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_10 | GPIO_Pin_11;
+      GPIO_InitStructure.GPIO_Mode = 0x04; // defined as IN_FLOATING?
+      GPIO_Init(GPIOC, &GPIO_InitStructure);
     break;
   case 2:
     //2-unhang all except input [where to re-hang-use a flag]+1 extra option: clocks hang/clocks unhang here
     // input is pb7
     hangflag=1;
     //  GPIO_Init(GPIOB, &GPIO_InitStructure);
-      GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_8 | GPIO_Pin_9 ;
-  GPIO_InitStructure.GPIO_Mode = 0x04;
+          GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_8 | GPIO_Pin_9 ;
+    GPIO_InitStructure.GPIO_Mode = 0x04;
   GPIO_Init(GPIOB, &GPIO_InitStructure);
-  GPIO_Init(GPIOC, &GPIO_InitStructure);
+  //  GPIO_Init(GPIOC, &GPIO_InitStructure);
   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_10 | GPIO_Pin_11;
   GPIO_InitStructure.GPIO_Mode = 0x04; // defined as IN_FLOATING?
   GPIO_Init(GPIOC, &GPIO_InitStructure);
@@ -330,6 +330,25 @@ void switch_jack(void)
   //  GPIOC->ODR |= (1<<10);
 
 
+}
+
+void reset_switches(void)
+{
+  GPIO_InitTypeDef  GPIO_InitStructure;
+
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9 ; // this works for pin 4 (should use for all pins?)
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+  GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_10 | GPIO_Pin_11;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
 }
 
 void setup_switches(void)
